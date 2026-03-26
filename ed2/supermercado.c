53dea0257616dfce73aef77eacddb1bc73e9cc4b
pybiCassi <cassianodallostoo@gmail.com>
2026-03-26 18:23:49 -0300

    feito

--- ed2/supermercado.c
+++ ed2/supermercado.c
@@ -21,8 +21,8 @@ typedef struct fila { //estrutura da fila
 
 typedef struct caixa{
     fila *fila;
-    cliente *cliente_atual;
-    int tempo_restante;
+    cliente *clienteAtual;
+    int tempoRestante;
 }caixa;
 
 typedef struct loja{
@@ -34,6 +34,16 @@ fila* criaFila(){
     f->inicio = NULL;
     f->fim = NULL;
     return f;
+}
+
+ loja* criaLoja(){
+    loja *l = (loja*)malloc(sizeof(loja));
+    for(int i = 0; i < 5; i++){
+        l->caixas[i].fila = criaFila();
+        l->caixas[i].clienteAtual = NULL;
+        l->caixas[i].tempoRestante = 0;
+    }
+    return l;
 }
 
 void insereFila(fila* f, cliente* c){
@@ -53,6 +63,7 @@ void insereFila(fila* f, cliente* c){
 void removeFila(fila* f){
     if(f->inicio == NULL || f->fim == NULL){
         printf("fila vazia");
+        return;
     }
     nodo *aux = f->inicio;
     f->inicio = f->inicio->prox;    
@@ -68,10 +79,11 @@ void imprimeFila(fila* f){
     printf("\n");
 }
 
-    bool verificaChegou(){
+    bool verificaChegou(){ //ve se chegou um cliente com a chance de 10%
 
     int num = rand()%100;
-    if(num<=10){
+    if(num<=60){
+    
         return true;
     }   
     else {
@@ -79,7 +91,7 @@ void imprimeFila(fila* f){
     }
 }
 
-int contadorFila(fila* f){
+int contadorFila(fila* f){ //conta a quantidade de clientes na fila e retorna um inteiro com a quantidade
     int i = 0;
     nodo *aux = f->inicio;
     while(aux != NULL){
@@ -89,7 +101,7 @@ int contadorFila(fila* f){
     return i;
 }
 
-retornaMenorCaixa(loja* l){
+int retornaMenorCaixa(loja* l){ //indica a caixa com a menor fila da loja no parametro
     int menor = 0;
     for(int i = 1; i < 5; i++){
         if(contadorFila(l->caixas[i].fila) < contadorFila(l->caixas[menor].fila)){
@@ -100,20 +112,20 @@ retornaMenorCaixa(loja* l){
 
 }
 
-cliente* criarCliente(int id){
+cliente* criarCliente(int id){ //cria o cleinte com produtos de 1 a 7 para satisfazer o tempo que deve ser entre 10 e 15 
     cliente *c = (cliente*)malloc(sizeof(cliente));
     c->id = id;
     c->produtos = rand()%7+ 1;
     c->tempo = 0;
     return c;
 }
-int defineTempo(cliente* c){
+int defineTempo(cliente* c){ //definindo o tempo de atendimento baseado nos produtos
     c->tempo = c->produtos * 2;
     return c->tempo;
 }
 
 
-void chegada(loja* l, int *idContador){
+void chegada(loja* l, int *idContador){ //quando chega cliente, cria o cliente, define o tempo e insere na fila com menor caixa
     if (verificaChegou()){
         cliente *c = criarCliente(*idContador);
         (*idContador)++;
@@ -123,11 +135,54 @@ void chegada(loja* l, int *idContador){
         insereFila(l->caixas[caixaMenor].fila, c);
         printf("cliente %d na fila %d\n", c->id, caixaMenor);
     }
+}
 
+void atendimento(loja *l){
+
+for (int i = 0; i < 5; i++){
+    caixa *cx = &l->caixas[i];
+    if(cx->clienteAtual == NULL){
+        if(cx->fila->inicio != NULL){
+            cx->clienteAtual = cx->fila->inicio->c;
+            cx->tempoRestante = cx->clienteAtual->tempo;
+            removeFila(cx->fila);
+            printf("cliente %d sendoatendido na caixa %d\n", cx->clienteAtual->id, i);
+        }
+    }
+    
+    if(cx->clienteAtual != NULL){
+            cx->tempoRestante--;
+            if(cx->tempoRestante <= 0){
+                printf("cliente %d terminou de ser atendido na caixa %d\n", cx->clienteAtual->id, i);
+                free(cx->clienteAtual);
+                cx->clienteAtual = NULL;
+            }
+      }
+    }
+}
+
+void imprimeLoja(loja* l){
+    for(int i = 0; i<5; i++){
+        printf("caixa %d: ", i);
+        if(l->caixas[i].clienteAtual != NULL){
+            printf("atendendo: cliente %d /// tempo restante: %d | ", l->caixas[i].clienteAtual->id, l->caixas[i].tempoRestante);
+        }
+        printf("fila: ");
+        imprimeFila(l->caixas[i].fila);
+    }
 }
 int main() {
   srand(time(NULL));
-    
-    //printf("bool: %d\n", chegou());
-    return 0;
+  loja *loja = criaLoja();
+  int idContador = 1;
+  while(true){
+  chegada(loja, &idContador);
+  atendimento(loja);
+  imprimeLoja(loja);  
+  printf("pressiona enter para passar temmpo\n");
+
+  getchar();
+}
+
+return 0;
 }
